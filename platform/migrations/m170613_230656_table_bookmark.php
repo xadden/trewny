c7d8d37faa4a6d2a3fbd297316799d0cf25bb5b8
André Echevarria <echevarriandre@gmail.com>
2017-06-14 00:11:14 +0100

    table bookmark migration

---
+++ platform/migrations/m170613_230656_table_bookmark.php
@@ -0,0 +1,30 @@
+
+<?php
+
+/*
+ * m170613_230656_table_bookmark.php
+ * 2017 */
+
+use yii\db\Migration;
+
+class m170613_230656_table_bookmark extends Migration {
+    
+    /**
+     * @inheritdoc
+     */
+    public function up() {
+        $this->createTable('Bookmark', [
+            'id' => $this->primaryKey(),
+            'title' => $this->string()->notNull(),
+            'link' => $this->string()->notNull(),
+            'image' => $this->string()->notNull()
+        ], 'ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down() {
+        $this->dropTable('Bookmark');
+    }
+}
